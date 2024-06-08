bb3d22222731c3c1222194d40a02bdab792119bb
Crystweb <remont.moy-pc@yandex.ru>
2024-06-08 20:49:33 +0300

    add

--- merge_k_lists.py
+++ merge_k_lists.py
@@ -8,16 +8,13 @@ def merge_k_lists(lists):
     merged_list = []
     heap = []
 
-    # Додавання перших елементів з кожного списку в heap
     for i, lst in enumerate(lists):
         if lst:
             heapq.heappush(heap, (lst[0], i, 0))
 
-    # Обробка heap до повного злиття всіх списків
     while heap:
         val, list_idx, element_idx = heapq.heappop(heap)
         merged_list.append(val)
-        # Додавання наступного елемента зі списку, звідки був взятий поточний елемент
         if element_idx + 1 < len(lists[list_idx]):
             next_tuple = (lists[list_idx][element_idx + 1], list_idx, element_idx + 1)
             heapq.heappush(heap, next_tuple)
@@ -26,7 +23,10 @@ def merge_k_lists(lists):
 
 
 # Приклад використання
-lists = [[1, 4, 5], [1, 3, 4], [20, 6], [50, 13, 84], [200, 600, 87]]
+lists = [[1, 4, 5], [1, 3, 4], [2, 6]]
 merged_list = merge_k_lists(lists)
 print("Відсортований список:", merged_list)
+
+
+
 
